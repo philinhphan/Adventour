6329627d47b4155faf0c801d1fb001d1d506ac0b
JannikAkaNoProgrammer <jannik.n.graf@gmail.com>
2025-01-17 11:26:04 +0100

    Processing PopUp Animation

--- src/pages/ProcessingPage.jsx
+++ src/pages/ProcessingPage.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Popup from "../components/PopUp/PopUp";
 import Navbar from "../components/Navbar/Navbar";
 import "../assets/styles/ProcessingPage.css";
-import plane from "../assets/icons/planeIcon.svg";
+import plane from "../assets/icons/Notifcation.svg";
 
 /* import logo from "../assets/images/AdventourLogo.svg";
 import profil from "../assets/images/LisaProfil.jpg"; */
